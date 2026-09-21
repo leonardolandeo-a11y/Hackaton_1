f4e88eee04eaca68cc11d6f3cab0f509f06fddd1
Leonard <leonardo.landeo@utec.edu.pe>
2026-09-21 10:32:02 -0500

    First Commit

---
+++ src/test/java/com/example/hackaton_1/Hackaton1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackaton_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackaton1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
