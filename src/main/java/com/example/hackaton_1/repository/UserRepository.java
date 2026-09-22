3ba00784d2dd213de99944fcd30a2b7907d0c789
Leonard <leonardo.landeo@utec.edu.pe>
2026-09-21 19:13:41 -0500

    security

--- src/main/java/com/example/hackaton_1/repository/UserRepository.java
+++ src/main/java/com/example/hackaton_1/repository/UserRepository.java
@@ -1,14 +1,12 @@
 package com.example.hackaton_1.repository;
 
-
 import com.example.hackaton_1.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository
+        extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
